a9a50554e9d43cbe7db952feefb5c861d95ca3f5
hichem rtibi <hichem.rtibi@gmail.com>
2019-03-12 02:10:02 +0100

    first commit

---
+++ src/main/java/com/hichem/rtibi/TodoBackendApplication.java
@@ -0,0 +1,15 @@
+package com.hichem.rtibi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoBackendApplication.class, args);
+		
+		//first coomit
+	}
+
+}
